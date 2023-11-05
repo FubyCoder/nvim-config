37247498ce03a542493d35dfcdc8c6c25699558b
FubyCoder <fabio.cavallari.dev@gmail.com>
2023-11-05 19:42:47 +0100

    feat: added nvim-tresitter-context, added keybind for search with text

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
 
 	-- use({ 'rose-pine/neovim', as = 'rose-pine' })  -- This Color is disabled atm
 	use "lukas-reineke/indent-blankline.nvim"
+	use "nvim-treesitter/nvim-treesitter-context"
 
 	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
